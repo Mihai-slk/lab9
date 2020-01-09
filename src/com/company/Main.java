0efa1bc50d3018c2f725c38af8317e2efe2ea227
Mihai <sandru.mihai@hotmail.com>
2020-01-09 18:33:59 +0200

    tema lab 9 v2.0

---
+++ src/com/company/Main.java
@@ -0,0 +1,85 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Buna ziua.");
+        System.out.println("Numele meu este Mihai.");
+
+        System.out.println("Suma numerelor 15 si 10 este...");
+        int suma;
+        suma = 25;
+        short a = 15;
+        short b = 10;
+        float total = a + b;
+        String bugettotal = "total";
+        System.out.println(total);
+
+
+        System.out.println("Rezultatul impartirii numarului 36 la 9 este...");
+        int imp;
+        imp = 30;
+                short c = 36;
+                short d = 9;
+                float rezultat = c / d;
+                String ces = "resultat";
+                System.out.println(rezultat);
+
+
+        System.out.println("Rezultatul de la punctul A este:");
+        int ex;
+        ex = 43;
+        short a2 = 5;
+        short b2 = 8;
+        short c2 = 6;
+        float opr = (c2 * b2) - a2;
+        System.out.println(opr);
+
+
+        System.out.println("Rezultatul de la punctul B este:");
+        int ex1;
+        ex1 = 43;
+        short a3 = 55;
+        short b3 = 9;
+        short c3 = 9;
+        float opr1;
+        opr1 = (a3 + b3) % c3;
+        System.out.println(opr1);
+
+
+        System.out.println("Rezultatul de la punctul C este:");
+        int ex2;
+        ex2 = 43;
+        short a4 = 20;
+        short b4 = -3;
+        short c4 = 5;
+        short d4 = 8;
+        float opr2;
+        opr2 = a4 + ( b4 * c4) / d4;
+        System.out.println(opr2);
+
+
+        System.out.println("Rezultatul de la punctul D este:");
+        int ex3;
+        ex3 = 43;
+        short a5 = 5;
+        short b5 = 15;
+        short c5 = 3;
+        short d5 = 2;
+        short e5 =8;
+        short f5 =3;
+        float opr3;
+        opr3 = (a5 + ((b5 / c5) * d5)) - (e5 % f5);
+        System.out.println(opr3);
+
+
+
+
+
+
+
+    }
+
+
+}
+
